d93dffc43e0f530f8099ddba344f4aeed0301d0c
mpeits <mpeits@tuta.io>
2025-01-28 13:41:38 +0200

    characters are counted

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+characters = {
+
+}
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
 
 word_count = len(file_contents.split())
+words = file_contents.split()
+
+for i in words:
+    for j in i:
+        j = j.lower()
+        if j in characters:
+            characters[j] += 1
+        else:
+            characters[j] = 1
 
 print(word_count)
+print(characters)
